46b556fe124749eb73ae8a0795d3e0848cfd387d
NBzT <nixondgomez100@hotmail.com>
2026-02-06 17:15:07 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+Numero1 = float(input("Ingrese el primer número a sumar: ")) 
+Numero2 = float(input("Ingrese el segundo número: "))
+
+Suma = Numero1 + Numero2
+
+print("La suma es igual a:", suma)
